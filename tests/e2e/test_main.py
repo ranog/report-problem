91f9f28fd145290f4c0ffb296c54ccb8b59a8e8b
ranog <jprnogueira@yahoo.com.br>
2023-02-11 19:27:18 -0300

    start basic infrastructure

---
+++ tests/e2e/test_main.py
@@ -0,0 +1,7 @@
+from httpx import AsyncClient
+
+
+async def test_it_should_ping_successfully(async_http_client: AsyncClient):
+    response = await async_http_client.get('/v1/ping/')
+    assert response.status_code == 200
+    assert response.json() == {'ping': 'pong'}
