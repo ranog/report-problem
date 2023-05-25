fbfb9ac58786bc21c87591348d2497fdc66e2afb
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-24 22:51:43 -0300

    implement endpoint for new issue

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from google.cloud import firestore
+
+
+COLLECTION_NAME = 'testing-report-problem'
 
 
 app = FastAPI()
@@ -7,3 +11,11 @@ app = FastAPI()
 @app.get('/v1/ping/')
 async def root():
     return {'ping': 'pong'}
+
+
+@app.post('/v1/new-issue/')
+async def new_issue(json: dict):
+    collection = firestore.AsyncClient().collection(COLLECTION_NAME)
+    await collection.document(str(id(json['user_id']))).set(json)
+    issues = await collection.where(field_path='user_id', op_string='==', value=json['user_id']).get()
+    return Response(headers={'location': issues[0].id}, status_code=200)
--- tests/e2e/test_main.py
+++ tests/e2e/test_main.py
@@ -1,7 +1,30 @@
+from datetime import datetime
+
 from httpx import AsyncClient
+
+from src.main import COLLECTION_NAME
+
+
+TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
 
 async def test_it_should_ping_successfully(async_http_client: AsyncClient):
     response = await async_http_client.get('/v1/ping/')
     assert response.status_code == 200
     assert response.json() == {'ping': 'pong'}
+
+
+async def test_it_should_shorten_successfully(clean_collection, async_http_client: AsyncClient):
+    await clean_collection(COLLECTION_NAME)
+    payload = {
+        'user_id': 1,
+        'email': 'test@email.com',
+        'description': 'dummy description',
+        'category': 'dummy category',
+        'priority': 'high',
+        'date': datetime.utcnow().strftime(TIMESTAMP_FORMAT),
+    }
+    expected_issue_id = str(id(payload['user_id']))
+    response = await async_http_client.post('/v1/new-issue/', json=payload)
+    assert response.status_code == 200
+    assert response.headers['location'] == expected_issue_id
