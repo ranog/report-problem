6b04b2c7bf4c00f2d4fff444e635ef6fe679e30c
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-25 16:28:29 -0300

    repository interface implementation

---
+++ src/issue_repository.py
@@ -0,0 +1,22 @@
+import uuid
+
+from google.cloud import firestore
+
+
+COLLECTION_NAME = 'testing-report-problem'
+
+
+class IssueRepository:
+    def __init__(self):
+        self.collection = firestore.AsyncClient().collection(COLLECTION_NAME)
+
+    async def add(self, issue: dict):
+        issue['id'] = str(uuid.uuid4())
+        await self.collection.document().set(issue)
+        return issue['id']
+
+    async def get_issue(self, issue_id: str):
+        issues = await self.collection.where(field_path='id', op_string='==', value=issue_id).get()
+        if issues:
+            return issues[0].to_dict()
+        return ''
