053fc0d90d2f73aeaa694f88803bfa15a663f958
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-05-25 22:01:46 -0300

    add check when new issue comes empty

--- src/main.py
+++ src/main.py
@@ -17,5 +17,7 @@ async def root():
 
 @app.post('/v1/report-new-issue/')
 async def report_new_issue(json: dict):
-    issue_id = await IssueRepository().add(json)
-    return JSONResponse(issue_id, status_code=200)
+    if json:
+        issue_id = await IssueRepository().add(json)
+        return JSONResponse(issue_id, status_code=200)
+    return JSONResponse('New issue cannot be empty', status_code=400)
--- tests/e2e/test_main.py
+++ tests/e2e/test_main.py
@@ -13,3 +13,13 @@ async def test_it_should_successfully_create_a_new_issue(issue, clean_collection
     await clean_collection(COLLECTION_NAME)
     response = await async_http_client.post('/v1/report-new-issue/', json=issue)
     assert response.status_code == 200
+
+
+async def test_it_should_return_status_code_400_when_the_new_issue_is_empty(
+    clean_collection,
+    async_http_client: AsyncClient,
+):
+    await clean_collection(COLLECTION_NAME)
+    response = await async_http_client.post('/v1/report-new-issue/', json={})
+    assert response.status_code == 400
+    assert response.json() == 'New issue cannot be empty'
