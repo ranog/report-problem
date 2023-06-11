0d8f001fd17e025be019405629814ad8ee7b614c
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-06-11 03:03:08 -0300

    notebook check refactoring

--- src/service.py
+++ src/service.py
@@ -1,24 +1,26 @@
 from src.model import NotebookCheck, Priority
 
 
-def check_notebook(notebook_check: NotebookCheck):
-    if (
-        notebook_check.it_is_not_turning_on
-        or notebook_check.power_button_is_not_working
-        or notebook_check.screen_is_not_working
-    ):
+def check_notebook(notebook_check: NotebookCheck) -> Priority:
+    high_priority = [
+        notebook_check.it_is_not_turning_on,
+        notebook_check.power_button_is_not_working,
+        notebook_check.screen_is_not_working,
+    ]
+    medium_priority = [
+        notebook_check.keyboard_is_not_working,
+        notebook_check.touchpad_is_not_working,
+        notebook_check.not_connecting_to_the_internet,
+    ]
+    low_priority = [
+        notebook_check.displays_error_message,
+        notebook_check.does_not_recognize_peripherals,
+        notebook_check.operating_system_does_not_start_correctly,
+    ]
+
+    if any(high_priority):
         return Priority.HIGH
-
-    if (
-        notebook_check.keyboard_is_not_working
-        or notebook_check.touchpad_is_not_working
-        or notebook_check.not_connecting_to_the_internet
-    ):
+    if any(medium_priority):
         return Priority.MEDIUM
-
-    if (
-        notebook_check.displays_error_message
-        or notebook_check.does_not_recognize_peripherals
-        or notebook_check.operating_system_does_not_start_correctly
-    ):
+    if any(low_priority):
         return Priority.LOW
