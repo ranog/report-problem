4c682453520108af9c129ada862d3a052295674f
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-06-16 11:16:38 -0300

    fix integration tests

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -1,11 +1,11 @@
 import os
-from datetime import datetime, timezone
 
 import pytest
 from google.api_core.exceptions import NotFound
 
-from src.model import Category, Notebook, Priority, Software, Status
+from src.model import Category, Notebook, Peripheral, Priority, Software, Status
 from src.repository import COLLECTION_NAME, IssueRepository
+from src.service import select_priority
 
 
 async def test_it_should_persist_in_the_repository(clean_collection, notebook_payload):
@@ -40,23 +40,81 @@ async def test_it_should_return_an_empty_dict_when_the_issue_id_does_not_exist_i
     assert issue == {}
 
 
-async def test_it_should_return_list_of_issues_when_given_correct_parameters(software_payload, clean_collection):
+async def test_it_should_return_list_of_issues_when_given_correct_parameters(clean_collection):
     await clean_collection(COLLECTION_NAME)
-    payload_1 = Software(username='dummy name one', **software_payload)
-    payload_1.it_is_not_installed_correctly = True
-    payload_2 = Software(username='dummy name two', **software_payload)
-    payload_2.not_displaying_data_and_content_correctly = True
-    payload_3 = Software(username='dummy name three', **software_payload)
-    payload_3.generates_unexpected_results = True
+    payload_1 = {
+        'username': 'dummy name',
+        'user_id': '99999999999999',
+        'user_email': 'user@email.com',
+        'contact_phone': 'AA 9NNNN-NNNN',
+        'description': 'dummy description',
+        'software_name': 'Dummy Software',
+        'it_is_not_installed_correctly': True,
+        'run_with_errors': False,
+        'does_not_respond_to_commands_and_interactions': False,
+        'not_displaying_data_and_content_correctly': False,
+        'generates_unexpected_results': False,
+        'not_integrating_with_other_systems_or_devices': False,
+        'not_using_required_system_resources': False,
+        'not_maintaining_security_and_not_protecting_data': False,
+        'it_is_not_updated_with_the_latest_versions': False,
+        'other_users_are_having_the_same_problem': False,
+    }
+    payload_2 = {
+        'username': 'dummy name',
+        'user_id': '111111111',
+        'user_email': 'user@email.com',
+        'contact_phone': 'AA 9NNNN-NNNN',
+        'description': 'dummy description',
+        'software_name': 'Dummy Software',
+        'it_is_not_installed_correctly': False,
+        'run_with_errors': False,
+        'does_not_respond_to_commands_and_interactions': False,
+        'not_displaying_data_and_content_correctly': True,
+        'generates_unexpected_results': False,
+        'not_integrating_with_other_systems_or_devices': False,
+        'not_using_required_system_resources': False,
+        'not_maintaining_security_and_not_protecting_data': False,
+        'it_is_not_updated_with_the_latest_versions': False,
+        'other_users_are_having_the_same_problem': False,
+    }
+    payload_3 = {
+        'username': 'dummy name',
+        'user_id': '55',
+        'user_email': 'user@email.com',
+        'contact_phone': 'AA 9NNNN-NNNN',
+        'description': 'dummy description',
+        'software_name': 'Dummy Software',
+        'it_is_not_installed_correctly': False,
+        'run_with_errors': False,
+        'does_not_respond_to_commands_and_interactions': False,
+        'not_displaying_data_and_content_correctly': False,
+        'generates_unexpected_results': True,
+        'not_integrating_with_other_systems_or_devices': False,
+        'not_using_required_system_resources': False,
+        'not_maintaining_security_and_not_protecting_data': False,
+        'it_is_not_updated_with_the_latest_versions': False,
+        'other_users_are_having_the_same_problem': False,
+    }
+    software_problem_1 = Software(**payload_1)
+    software_problem_2 = Software(**payload_2)
+    software_problem_3 = Software(**payload_3)
+    software_problem_1.priority = select_priority(software_problem_1)
+    software_problem_2.priority = select_priority(software_problem_2)
+    software_problem_3.priority = select_priority(software_problem_3)
+
     repository = IssueRepository()
-    await repository.add(payload_1)
-    await repository.add(payload_2)
-    await repository.add(payload_3)
+    await repository.add(software_problem_1)
+    await repository.add(software_problem_2)
+    await repository.add(software_problem_3)
+
+    software_problem_2.created_at = str(software_problem_2.created_at)
+    software_problem_3.created_at = str(software_problem_3.created_at)
 
     issues = await repository.list(category=Category.SOFTWARE.value, priority=Priority.MEDIUM.value)
 
     assert len(issues) == 2
-    assert issues == [payload_2, payload_3]
+    assert issues == [software_problem_2.dict(), software_problem_3.dict()]
 
 
 @pytest.mark.parametrize(
@@ -84,77 +142,112 @@ async def test_it_should_return_items_from_the_given_category(clean_collection):
     await clean_collection(COLLECTION_NAME)
     payload_1 = {
         'username': 'dummy name',
-        'user_id': '99999999999999',
-        'user_email': 'user_2@email.com',
+        'user_id': '111111111',
+        'user_email': 'user@email.com',
         'contact_phone': 'AA 9NNNN-NNNN',
         'description': 'dummy description',
-        'category': Category.NOTEBOOK.value,
-        'priority': Priority.LOW.value,
-        'created_at': str(datetime(2022, 1, 31, 10, 0, 0, tzinfo=timezone.utc)),
-        'status': Status.TO_DO.value,
-        'responsible_engineer': 'other_specific@engineer.com',
+        'it_is_not_turning_on': True,
+        'power_button_is_not_working': False,
+        'screen_is_not_working': False,
+        'keyboard_is_not_working': False,
+        'touchpad_is_not_working': False,
+        'not_connecting_to_the_internet': False,
+        'displays_error_message': False,
+        'does_not_recognize_peripherals': False,
+        'operating_system_does_not_start_correctly': False,
     }
     payload_2 = {
-        'username': 'dummy name',
-        'user_id': '99999999999999',
-        'user_email': 'user_2@email.com',
-        'contact_phone': 'AA 9NNNN-NNNN',
-        'description': 'dummy description',
-        'category': Category.SOFTWARE.value,
-        'priority': Priority.MEDIUM.value,
-        'created_at': str(datetime(2022, 1, 31, 10, 0, 0, tzinfo=timezone.utc)),
-        'status': Status.TO_DO.value,
-        'responsible_engineer': 'other_specific@engineer.com',
-    }
-    payload_3 = {
         'username': 'dummy name',
         'user_id': '111111111',
         'user_email': 'user@email.com',
         'contact_phone': 'AA 9NNNN-NNNN',
         'description': 'dummy description',
-        'category': Category.SOFTWARE.value,
-        'priority': Priority.HIGH.value,
-        'created_at': str(datetime.now(timezone.utc)),
-        'status': Status.DONE.value,
-        'responsible_engineer': 'specific@engineer.com',
+        'software_name': 'Dummy Software',
+        'it_is_not_installed_correctly': False,
+        'run_with_errors': False,
+        'does_not_respond_to_commands_and_interactions': False,
+        'not_displaying_data_and_content_correctly': True,
+        'generates_unexpected_results': False,
+        'not_integrating_with_other_systems_or_devices': False,
+        'not_using_required_system_resources': False,
+        'not_maintaining_security_and_not_protecting_data': False,
+        'it_is_not_updated_with_the_latest_versions': False,
+        'other_users_are_having_the_same_problem': False,
     }
+    payload_3 = {
+        'username': 'dummy name',
+        'user_id': '55',
+        'user_email': 'user@email.com',
+        'contact_phone': 'AA 9NNNN-NNNN',
+        'description': 'dummy description',
+        'software_name': 'Dummy Software',
+        'it_is_not_installed_correctly': False,
+        'run_with_errors': False,
+        'does_not_respond_to_commands_and_interactions': False,
+        'not_displaying_data_and_content_correctly': False,
+        'generates_unexpected_results': True,
+        'not_integrating_with_other_systems_or_devices': False,
+        'not_using_required_system_resources': False,
+        'not_maintaining_security_and_not_protecting_data': False,
+        'it_is_not_updated_with_the_latest_versions': False,
+        'other_users_are_having_the_same_problem': False,
+    }
+    notebook_problem_1 = Notebook(**payload_1)
+    software_problem_2 = Software(**payload_2)
+    software_problem_3 = Software(**payload_3)
+    notebook_problem_1.priority = select_priority(notebook_problem_1)
+    software_problem_2.priority = select_priority(software_problem_2)
+    software_problem_3.priority = select_priority(software_problem_3)
+
     repository = IssueRepository()
-    await repository.add(payload_1)
-    await repository.add(payload_2)
-    await repository.add(payload_3)
+    await repository.add(notebook_problem_1)
+    await repository.add(software_problem_2)
+    await repository.add(software_problem_3)
+
+    software_problem_2.created_at = str(software_problem_2.created_at)
+    software_problem_3.created_at = str(software_problem_3.created_at)
 
     issues = await repository.list(category=Category.SOFTWARE.value)
 
     assert len(issues) == 2
-    assert issues == [payload_2, payload_3]
+    assert issues == [software_problem_2, software_problem_3]
 
 
 async def test_it_should_return_items_of_given_priority(clean_collection):
     await clean_collection(COLLECTION_NAME)
     payload_1 = {
         'username': 'dummy name',
-        'user_id': '99999999999999',
-        'user_email': 'user_2@email.com',
+        'user_id': '111111111',
+        'user_email': 'user@email.com',
         'contact_phone': 'AA 9NNNN-NNNN',
         'description': 'dummy description',
-        'category': Category.SOFTWARE.value,
-        'priority': Priority.HIGH.value,
-        'created_at': str(datetime(2022, 1, 31, 10, 0, 0, tzinfo=timezone.utc)),
-        'status': Status.TO_DO.value,
-        'responsible_engineer': 'other_specific@engineer.com',
+        'it_is_not_turning_on': True,
+        'power_button_is_not_working': False,
+        'screen_is_not_working': False,
+        'keyboard_is_not_working': False,
+        'touchpad_is_not_working': False,
+        'not_connecting_to_the_internet': False,
+        'displays_error_message': False,
+        'does_not_recognize_peripherals': False,
+        'operating_system_does_not_start_correctly': False,
     }
-
     payload_2 = {
         'username': 'dummy name',
-        'user_id': '99999999999999',
-        'user_email': 'user_2@email.com',
+        'user_id': '111111111',
+        'user_email': 'user@email.com',
         'contact_phone': 'AA 9NNNN-NNNN',
         'description': 'dummy description',
-        'category': Category.SOFTWARE.value,
-        'priority': Priority.MEDIUM.value,
-        'created_at': str(datetime(2022, 1, 31, 10, 0, 0, tzinfo=timezone.utc)),
-        'status': Status.TO_DO.value,
-        'responsible_engineer': 'other_specific@engineer.com',
+        'software_name': 'Dummy Software',
+        'it_is_not_installed_correctly': False,
+        'run_with_errors': False,
+        'does_not_respond_to_commands_and_interactions': False,
+        'not_displaying_data_and_content_correctly': False,
+        'generates_unexpected_results': False,
+        'not_integrating_with_other_systems_or_devices': False,
+        'not_using_required_system_resources': False,
+        'not_maintaining_security_and_not_protecting_data': False,
+        'it_is_not_updated_with_the_latest_versions': False,
+        'other_users_are_having_the_same_problem': True,
     }
     payload_3 = {
         'username': 'dummy name',
@@ -162,25 +255,41 @@ async def test_it_should_return_items_of_given_priority(clean_collection):
         'user_email': 'user@email.com',
         'contact_phone': 'AA 9NNNN-NNNN',
         'description': 'dummy description',
-        'category': Category.SOFTWARE.value,
-        'priority': Priority.HIGH.value,
-        'created_at': str(datetime.now(timezone.utc)),
-        'status': Status.DONE.value,
-        'responsible_engineer': 'specific@engineer.com',
+        'peripheral_type': 'Dummy Peripheral',
+        'does_not_connect': True,
+        'operating_system_is_not_recognizing': False,
+        'does_not_work_without_displaying_errors_or_failure_messages': False,
+        'does_not_respond_to_commands': False,
+        'does_not_perform_its_main_functions': False,
+        'does_not_integrate_with_other_devices_or_components': False,
+        'does_not_receive_power_or_is_not_turned_on': False,
+        'is_not_up_to_date_with_the_latest_versions_of_drivers_or_firmware': False,
+        'other_users_are_using_the_same_peripheral_and_are_having_the_same_problem': False,
+        'does_not_maintain_data_security_and_protection': False,
     }
-    repository = IssueRepository()
-    await repository.add(payload_1)
-    await repository.add(payload_2)
-    await repository.add(payload_3)
+    notebook_problem_1 = Notebook(**payload_1)
+    software_problem_2 = Software(**payload_2)
+    peripheral_problem_3 = Peripheral(**payload_3)
+    notebook_problem_1.priority = select_priority(notebook_problem_1)
+    software_problem_2.priority = select_priority(software_problem_2)
+    peripheral_problem_3.priority = select_priority(peripheral_problem_3)
 
+    repository = IssueRepository()
+    await repository.add(notebook_problem_1)
+    await repository.add(software_problem_2)
+    await repository.add(peripheral_problem_3)
+
+    notebook_problem_1.created_at = str(notebook_problem_1.created_at)
+    peripheral_problem_3.created_at = str(peripheral_problem_3.created_at)
     issues = await repository.list(priority=Priority.HIGH.value)
 
     assert len(issues) == 2
-    assert issues == [payload_1, payload_3]
+    assert issues == [notebook_problem_1, peripheral_problem_3]
 
 
-async def test_it_should_update_all_fields_provided(new_issue, clean_collection):
+async def test_it_should_update_all_fields_provided(notebook_payload, clean_collection):
     await clean_collection(COLLECTION_NAME)
+    new_issue = Notebook(**notebook_payload)
     repository = IssueRepository()
     issue_id = await repository.add(new_issue)
     item_to_update = {
