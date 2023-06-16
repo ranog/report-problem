0e44a858b6739d31c6a774b5c207ceacb885aae0
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-06-16 11:42:31 -0300

    fix service unit tests

--- tests/unit/service/test_check_notebook.py
+++ tests/unit/service/test_check_notebook.py
@@ -1,56 +1,65 @@
-from src.model import Priority
+from src.model import Notebook, Priority
 from src.service import select_priority
 
 
-def test_it_should_return_high_priority_when_notebook_will_not_turn_on(notebook_check):
-    notebook_check['it_is_not_turning_on'] = True
-    priority = select_priority(notebook_check)
-    assert priority == Priority.HIGH
+def test_it_should_return_high_priority_when_notebook_will_not_turn_on(notebook_payload):
+    notebook_payload['it_is_not_turning_on'] = True
+    notebook_problem = Notebook(**notebook_payload)
+    priority = select_priority(notebook_problem)
+    assert priority == Priority.HIGH.value
 
 
-def test_it_should_return_high_priority_when_power_button_is_not_working(notebook_check):
-    notebook_check['power_button_is_not_working'] = True
-    priority = select_priority(notebook_check)
-    assert priority == Priority.HIGH
+def test_it_should_return_high_priority_when_power_button_is_not_working(notebook_payload):
+    notebook_payload['power_button_is_not_working'] = True
+    notebook_problem = Notebook(**notebook_payload)
+    priority = select_priority(notebook_problem)
+    assert priority == Priority.HIGH.value
 
 
-def test_it_should_return_high_priority_when_screen_is_not_working(notebook_check):
-    notebook_check['screen_is_not_working'] = True
-    priority = select_priority(notebook_check)
-    assert priority == Priority.HIGH
+def test_it_should_return_high_priority_when_screen_is_not_working(notebook_payload):
+    notebook_payload['screen_is_not_working'] = True
+    notebook_problem = Notebook(**notebook_payload)
+    priority = select_priority(notebook_problem)
+    assert priority == Priority.HIGH.value
 
 
-def test_it_should_return_medium_priority_when_keyboard_is_not_working(notebook_check):
-    notebook_check['keyboard_is_not_working'] = True
-    priority = select_priority(notebook_check)
-    assert priority == Priority.MEDIUM
+def test_it_should_return_medium_priority_when_keyboard_is_not_working(notebook_payload):
+    notebook_payload['keyboard_is_not_working'] = True
+    notebook_problem = Notebook(**notebook_payload)
+    priority = select_priority(notebook_problem)
+    assert priority == Priority.MEDIUM.value
 
 
-def test_it_should_return_medium_priority_when_touchpad_is_not_working(notebook_check):
-    notebook_check['touchpad_is_not_working'] = True
-    priority = select_priority(notebook_check)
-    assert priority == Priority.MEDIUM
+def test_it_should_return_medium_priority_when_touchpad_is_not_working(notebook_payload):
+    notebook_payload['touchpad_is_not_working'] = True
+    notebook_problem = Notebook(**notebook_payload)
+    priority = select_priority(notebook_problem)
+    assert priority == Priority.MEDIUM.value
 
 
-def test_it_should_return_medium_priority_when_not_connecting_to_the_internet(notebook_check):
-    notebook_check['not_connecting_to_the_internet'] = True
-    priority = select_priority(notebook_check)
-    assert priority == Priority.MEDIUM
+def test_it_should_return_medium_priority_when_not_connecting_to_the_internet(notebook_payload):
+    notebook_payload['not_connecting_to_the_internet'] = True
+    notebook_problem = Notebook(**notebook_payload)
+    priority = select_priority(notebook_problem)
+    assert priority == Priority.MEDIUM.value
 
 
-def test_it_should_return_low_priority_when_displaying_error_message(notebook_check):
-    notebook_check['displays_error_message'] = True
-    priority = select_priority(notebook_check)
-    assert priority == Priority.LOW
+def test_it_should_return_low_priority_when_displaying_error_message(notebook_payload):
+    notebook_payload['displays_error_message'] = True
+    notebook_problem = Notebook(**notebook_payload)
+    priority = select_priority(notebook_problem)
+    assert priority == Priority.LOW.value
 
 
-def test_it_should_return_low_priority_when_not_recognizing_peripherals(notebook_check):
-    notebook_check['does_not_recognize_peripherals'] = True
-    priority = select_priority(notebook_check)
-    assert priority == Priority.LOW
+def test_it_should_return_low_priority_when_not_recognizing_peripherals(notebook_payload):
+    notebook_payload['does_not_recognize_peripherals'] = True
+    notebook_problem = Notebook(**notebook_payload)
+    priority = select_priority(notebook_problem)
+    assert priority == Priority.LOW.value
 
 
-def test_it_should_return_low_priority_when_operating_system_fails_to_start_properly(notebook_check):
-    notebook_check['operating_system_does_not_start_correctly'] = True
-    priority = select_priority(notebook_check)
-    assert priority == Priority.LOW
+def test_it_should_return_low_priority_when_operating_system_fails_to_start_properly(notebook_payload):
+    notebook_payload['operating_system_does_not_start_correctly'] = True
+    notebook_problem = Notebook(**notebook_payload)
+    priority = select_priority(notebook_problem)
+    assert priority == Priority.LOW.value
--- tests/unit/service/test_check_software.py
+++ tests/unit/service/test_check_software.py
@@ -1,62 +1,72 @@
-from src.model import Priority
+from src.model import Priority, Software
 from src.service import select_priority
 
 
-def test_it_should_return_high_priority_when_not_properly_installed(software_check):
-    software_check['it_is_not_installed_correctly'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.HIGH
+def test_it_should_return_high_priority_when_not_properly_installed(software_payload):
+    software_payload['it_is_not_installed_correctly'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.HIGH.value
 
 
-def test_it_should_return_high_priority_when_run_with_errors(software_check):
-    software_check['run_with_errors'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.HIGH
+def test_it_should_return_high_priority_when_run_with_errors(software_payload):
+    software_payload['run_with_errors'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.HIGH.value
 
 
-def test_it_should_return_high_priority_when_not_responding_to_commands_and_interactions(software_check):
-    software_check['does_not_respond_to_commands_and_interactions'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.HIGH
+def test_it_should_return_high_priority_when_not_responding_to_commands_and_interactions(software_payload):
+    software_payload['does_not_respond_to_commands_and_interactions'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.HIGH.value
 
 
-def test_it_should_return_medium_priority_when_not_displaying_data_and_contents_correctly(software_check):
-    software_check['not_displaying_data_and_content_correctly'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.MEDIUM
+def test_it_should_return_medium_priority_when_not_displaying_data_and_contents_correctly(software_payload):
+    software_payload['not_displaying_data_and_content_correctly'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.MEDIUM.value
 
 
-def test_it_should_return_medium_priority_when_generating_unexpected_results(software_check):
-    software_check['generates_unexpected_results'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.MEDIUM
+def test_it_should_return_medium_priority_when_generating_unexpected_results(software_payload):
+    software_payload['generates_unexpected_results'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.MEDIUM.value
 
 
-def test_it_should_return_medium_priority_when_not_integrating_with_other_systems_or_devices(software_check):
-    software_check['not_integrating_with_other_systems_or_devices'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.MEDIUM
+def test_it_should_return_medium_priority_when_not_integrating_with_other_systems_or_devices(software_payload):
+    software_payload['not_integrating_with_other_systems_or_devices'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.MEDIUM.value
 
 
-def test_it_should_return_low_priority_when_not_using_required_system_resources(software_check):
-    software_check['not_using_required_system_resources'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.LOW
+def test_it_should_return_low_priority_when_not_using_required_system_resources(software_payload):
+    software_payload['not_using_required_system_resources'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.LOW.value
 
 
-def test_it_should_return_low_priority_when_not_maintaining_security_and_not_protecting_data(software_check):
-    software_check['not_maintaining_security_and_not_protecting_data'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.LOW
+def test_it_should_return_low_priority_when_not_maintaining_security_and_not_protecting_data(software_payload):
+    software_payload['not_maintaining_security_and_not_protecting_data'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.LOW.value
 
 
-def test_it_should_return_low_priority_when_not_updated_with_latest_versions(software_check):
-    software_check['it_is_not_updated_with_the_latest_versions'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.LOW
+def test_it_should_return_low_priority_when_not_updated_with_latest_versions(software_payload):
+    software_payload['it_is_not_updated_with_the_latest_versions'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.LOW.value
 
 
-def test_it_should_return_low_priority_when_other_users_have_the_same_problem(software_check):
-    software_check['other_users_are_having_the_same_problem'] = True
-    priority = select_priority(software_check)
-    assert priority == Priority.LOW
+def test_it_should_return_low_priority_when_other_users_have_the_same_problem(software_payload):
+    software_payload['other_users_are_having_the_same_problem'] = True
+    software_problem = Software(**software_payload)
+    priority = select_priority(software_problem)
+    assert priority == Priority.LOW.value
